4c4324b539fe651f4b2b30c3826a164405ccdc86
AliceGrr <lesskiig00@yandex.ru>
2021-02-15 19:42:26 +0300

    try to add migrations

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from . import routes, models
--- server_runner.py
+++ server_runner.py
@@ -1,5 +1,4 @@
-from server import app, db
+from server import app, db, manager, migrate
 
 if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+    manager.run()
