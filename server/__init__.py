fce3a7c2b927ede9be679975518986b3996d94d9
AliceGrr <lesskiig00@yandex.ru>
2021-03-31 18:39:01 +0300

    minor fixes

--- server/__init__.py
+++ server/__init__.py
@@ -1,20 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from flask_moment import Moment
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
 from . import models
 from . import routes
