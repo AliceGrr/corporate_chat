ad50f85c3123dcdb5262e1978a53b3d824e3d03b
AliceGrr <lesskiig00@yandex.ru>
2021-05-02 18:05:12 +0300

    try to deploy on heroku

--- server_runner.py
+++ server_runner.py
@@ -1,5 +1,6 @@
-from server import app, db
+import os
+from server import app
 
-if __name__ == '__main__':
-    db.create_all()
-    app.run(host='0.0.0.0')
+def run():
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
